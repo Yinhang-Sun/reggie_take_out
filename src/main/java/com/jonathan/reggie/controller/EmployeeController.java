c578bccb2e2e9b12b7faaaafe9de6baff636d6e8
yinhang <sunyinhang@gmail.com>
2024-04-01 11:59:29 -0700

    login and logout

--- src/main/java/com/jonathan/reggie/controller/EmployeeController.java
+++ src/main/java/com/jonathan/reggie/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.jonathan.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.jonathan.reggie.common.R;
+import com.jonathan.reggie.entity.Employee;
 import com.jonathan.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -13,4 +21,56 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * employee login
+     *
+     * @param request
+     * @param employee
+     * @return
+     */
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+
+        //1. Encrypt the password submitted on the page with md5
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //2. Query the database based on the username submitted on the page.
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        //3. If no query is found, the login failure result will be returned.
+        if(emp == null) {
+            return R.error("Login failed!");
+        }
+
+        //4. Password comparison, if inconsistent, return login failure result
+        if(!emp.getPassword().equals(password)) {
+            return R.error("Login failed!");
+        }
+
+        //5. Check the employee status. If it is disabled, return the employee disabled result.
+        if(emp.getStatus() == 0) {
+            return R.error("Account has been disabled");
+        }
+
+        //6. The login is successful, save the employee ID into the Session and return the login success result.
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
+
+    /**
+     * employee logout
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //Clear the id of the currently logged-in employee saved in the Session
+        request.getSession().removeAttribute("employee");
+        return R.success("Exit successfully");
+    }
 }
