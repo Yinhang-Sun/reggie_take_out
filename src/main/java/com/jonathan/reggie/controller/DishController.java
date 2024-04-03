e075a51171230c2ba5a6bafc6808c4f68ae011ed
yinhang <sunyinhang@gmail.com>
2024-04-03 14:36:32 -0700

    Dish pagination

--- src/main/java/com/jonathan/reggie/controller/DishController.java
+++ src/main/java/com/jonathan/reggie/controller/DishController.java
@@ -1,16 +1,23 @@
 package com.jonathan.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jonathan.reggie.common.R;
 import com.jonathan.reggie.dto.DishDto;
+import com.jonathan.reggie.entity.Category;
+import com.jonathan.reggie.entity.Dish;
+import com.jonathan.reggie.service.CategoryService;
 import com.jonathan.reggie.service.DishFlavorService;
 import com.jonathan.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * dish management
@@ -25,8 +32,12 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * add dish
+     *
      * @param dishDto
      * @return
      */
@@ -37,6 +48,57 @@ public class DishController {
         dishService.saveWithFlavor(dishDto);
 
         return R.success("Added dish successfully!");
+    }
+
+    /**
+     * dish info pagination
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+
+        //Construct pagination constructor
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        //conditional constructor
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        //adding filter
+        queryWrapper.like(name != null, Dish::getName, name);
+        //adding sorting
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        //execute query
+        dishService.page(pageInfo, queryWrapper);
+
+        //copy properties
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
+
+        List<Dish> records = pageInfo.getRecords();
+
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item, dishDto);
+
+            Long categoryId = item.getCategoryId();//category id
+            //Query category based on id
+            Category category = categoryService.getById(categoryId);
+
+            if (category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
     }
 
 
