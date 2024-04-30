fba74a698ad3924b3ba28abb26554f9833193ac3
yinhang <sunyinhang@gmail.com>
2024-04-29 22:46:47 -0700

    View orders (client)

--- src/main/java/com/jonathan/reggie/controller/OrderController.java
+++ src/main/java/com/jonathan/reggie/controller/OrderController.java
@@ -2,10 +2,13 @@ package com.jonathan.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jonathan.reggie.common.BaseContext;
 import com.jonathan.reggie.common.R;
 import com.jonathan.reggie.dto.OrdersDto;
+import com.jonathan.reggie.entity.OrderDetail;
 import com.jonathan.reggie.entity.Orders;
 import com.jonathan.reggie.entity.User;
+import com.jonathan.reggie.service.OrderDetailService;
 import com.jonathan.reggie.service.OrderService;
 import com.jonathan.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +31,9 @@ public class OrderController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
 
 
     /**
@@ -110,5 +116,65 @@ public class OrderController {
         orderService.updateById(orders);
 
         return R.success("Modified Successfully");
+    }
+
+    /**
+     * 查看手机版历史纪录
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/userPage")
+    public R<Page> pagePhone(int page,int pageSize){
+
+        // 新创返回类型Page
+        Page<Orders> pageInfo = new Page<>(page,pageSize);
+        Page<OrdersDto> ordersDtoPage = new Page<>();
+
+        // 用户ID
+        Long currentId = BaseContext.getCurrentId();
+
+        // 原条件写入
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Orders::getUserId,currentId);
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+
+        orderService.page(pageInfo,queryWrapper);
+
+        // 普通赋值
+        BeanUtils.copyProperties(pageInfo,ordersDtoPage,"records");
+
+        // 订单赋值
+        List<Orders> records = pageInfo.getRecords();
+
+        List<OrdersDto> ordersDtoList = records.stream().map((item) -> {
+
+            // 新创内部元素
+            OrdersDto ordersDto = new OrdersDto();
+
+            // 普通值赋值
+            BeanUtils.copyProperties(item,ordersDto);
+
+            // 菜单详情赋值
+            Long itemId = item.getId();
+
+            LambdaQueryWrapper<OrderDetail> orderDetailLambdaQueryWrapper = new LambdaQueryWrapper<>();
+            orderDetailLambdaQueryWrapper.eq(OrderDetail::getOrderId,itemId);
+
+            int count = orderDetailService.count(orderDetailLambdaQueryWrapper);
+
+            List<OrderDetail> orderDetailList = orderDetailService.list(orderDetailLambdaQueryWrapper);
+
+            ordersDto.setSumNum(count);
+
+            ordersDto.setOrderDetails(orderDetailList);
+
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        // 完成dishDtoPage的results的内容封装
+        ordersDtoPage.setRecords(ordersDtoList);
+
+        return R.success(ordersDtoPage);
     }
 }
--- src/main/java/com/jonathan/reggie/dto/OrdersDto.java
+++ src/main/java/com/jonathan/reggie/dto/OrdersDto.java
@@ -1,10 +1,31 @@
 package com.jonathan.reggie.dto;
 
+import com.jonathan.reggie.entity.OrderDetail;
 import com.jonathan.reggie.entity.Orders;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 public class OrdersDto extends Orders {
+    @Setter
+    @Getter
     private List<Orders> orders;
     private String userName;
+    @Setter
+    private Integer sumNum;
+    @Setter
+    @Getter
+    private List<OrderDetail> orderDetails;
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
 }
