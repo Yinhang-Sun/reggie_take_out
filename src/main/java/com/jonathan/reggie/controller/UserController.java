ae21eda8fa7d8d4839ee4e6e6d6a409ea90d1d7e
yinhang <sunyinhang@gmail.com>
2024-10-05 22:16:42 -0700

    test

--- src/main/java/com/jonathan/reggie/controller/UserController.java
+++ src/main/java/com/jonathan/reggie/controller/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
     public UserService userService;
 
     /**
-     * send phone SMS verification code
+     * send phone SMS verification code test
      * @param user
      * @return
      */
